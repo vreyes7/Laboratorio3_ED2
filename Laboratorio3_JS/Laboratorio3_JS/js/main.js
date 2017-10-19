1be5983159e7d9e1c3dc39656140fc0b8d7ecede
Luis Ovalle <luispalp@hotmail.com>
2017-10-18 22:44:07 -0600

    Se terminó la serie 1 y 2

--- Laboratorio3_JS/Laboratorio3_JS/js/main.js
+++ Laboratorio3_JS/Laboratorio3_JS/js/main.js
@@ -1,7 +1,7 @@
 ﻿// Your code here!
 
     var json_elementos = {};
-
+console.log("hola");
         function agregarObjeto() {
             var llave = document.getElementById(_llave);
             var valor = document.getElementById(_valor);
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Laboratorio3_ED2
+Debido a que no funcionaba en el editor de texto de Visual Studio se transfirio
+a un block de notas. El problema era que por seguridad del equipo no permitía
+ejecutar el script dentro del código.
