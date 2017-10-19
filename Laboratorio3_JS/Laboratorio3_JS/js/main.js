b0e0ff3f72233d24a523c760a5e8e2d1edfee2a6
Luis Ovalle <luispalp@hotmail.com>
2017-10-18 19:42:52 -0600

    Se ha creado el diseño html y empezado el script

--- Laboratorio3_JS/Laboratorio3_JS/js/main.js
+++ Laboratorio3_JS/Laboratorio3_JS/js/main.js
@@ -1,3 +1,13 @@
 ﻿// Your code here!
 
+    var json_elementos = {};
+
+        function agregarObjeto() {
+            var llave = document.getElementById(_llave);
+            var valor = document.getElementById(_valor);
+            json_elementos.push({ llave : valor });
+
+            
+        }
+
 
